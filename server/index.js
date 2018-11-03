9c7bf8a6756f41ca3db6b593e7ddbdf5e50df6d8
Nikhil  Chougule <nikhil.choughule@soroco.com>
2018-11-03 23:26:42 +0530

    twitter Signin api setup done

--- server/index.js
+++ server/index.js
@@ -1,11 +1,50 @@
 const express = require('express');
+const passport = require('passport');
+const TwitterStrategy = require('passport-twitter').Strategy;
+const PORT = process.env.PORT || 5000;
 
+passport.use(new TwitterStrategy({
+  consumerKey: process.env.CONSUMER_KEY,
+  consumerSecret: process.env.CONSUMER_SECRET,
+  callbackURL: `https://twitter-api-integration.herokuapp.com:${PORT}/login/twitter/return`
+},
+function(token, tokenSecret, profile, cb) {
+  // User.findOrCreate({ twitterId: profile.id }, function (err, user) {
+  //   return cb(err, user);
+  // });
+  return cb(null, profile);
+}));
+
+// Configure Passport authenticated session persistence.
+passport.serializeUser(function(user, cb) {
+  cb(null, user);
+});
+
+passport.deserializeUser(function(obj, cb) {
+  cb(null, obj);
+});
+
+
+// Create express app
 const app = express();
+
+
 
 // Route handler get , put, post, patch, delete
 app.get('/', (req, res) =>{
   res.send({ hi : 'there'})
 });
 
-const PORT = process.env.PORT || 5000;
+app.get('/auth/twitter',
+  passport.authenticate('twitter'));
+
+app.get('/auth/twitter/callback', 
+  passport.authenticate('twitter', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  }
+);
+
+// listen to port
 app.listen(PORT);
