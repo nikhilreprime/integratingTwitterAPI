7c02d1b7543511385466be2abd887147fef030ee
Nikhil  Chougule <nikhil.choughule@soroco.com>
2018-11-04 16:11:23 +0530

    front-end routes setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
+import Login from './scenes/Login/Login';
+import Profile from './scenes/Profile/Profile';
+import Home from './scenes/Home/Home';
+import NotFound from './components/PageNotFound/PageNotFound';
 
 class App extends Component {
+
   render() {
     return (
-      <div className="App">
-        
-      </div>
+      <Switch>
+        <Route exact path='/login' component={Login} />
+        <Route exact path="/profile/:id" component={Profile} />
+        <Route exact path='/' component={Home} />
+        <Route path='*' component={NotFound} />
+      </Switch>
     );
   }
 }
