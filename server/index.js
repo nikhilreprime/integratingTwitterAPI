de8982cb6df71a2fdc55639f4252f30cc0a064fd
Nikhil  Chougule <nikhil.choughule@soroco.com>
2018-11-04 19:14:43 +0530

    basic mongoose setup

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
-const express = require('express');
+require('dotenv').config();
 require('./services/passport');
+const express = require('express');
+const mongoose = require('mongoose')
 
 const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.MONGO_URI)
 
 // Create express app
 const app = express();
--- server/routes/profileRoutes.js
+++ server/routes/profileRoutes.js
@@ -1,9 +1,10 @@
 const passport = require('passport');
 
 module.exports = (app) => {
-  app.get('/profile/:id?',
+  app.get('/profile/:userId',
     passport.authenticate('twitter', { failureRedirect: '/login' }),
     (req, res) => {
+      var userId = req.params.userId;
       res.send({ message: 'Profile page' })
     });
 
