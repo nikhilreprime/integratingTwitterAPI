78ddf05c27974160a9faae10d30d86c9b7911532
Nikhil  Chougule <nikhil.choughule@soroco.com>
2018-11-05 10:59:57 +0530

    routing to react app

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 const express = require("express");
+const path = require('path');
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 5000;
@@ -12,7 +13,12 @@ const app = express();
 
 // Use application-level middleware for common functionality, including
 // logging, parsing, and session handling.
-app.use(require('serve-static')(__dirname + '/../build'));
+app.use(require('morgan')('combined'));
+
+
+// Serve static assets
+app.use(express.static(path.resolve(__dirname, '..', 'build')));
+
 app.use(require("cookie-parser")());
 app.use(require("body-parser").urlencoded({ extended: true }));
 app.use(
@@ -26,9 +32,16 @@ require("./services/passport")(app);
 
 
 //routes
+
+// Always return the main index.html, so react-router render the route in the client
 require("./routes/authRoutes")(app);
 require("./routes/homeRoutes")(app);
 require("./routes/profileRoutes")(app);
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
+});
+
+
 
 // listen to port
 app.listen(PORT);
