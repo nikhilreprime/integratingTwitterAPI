911125b904760f928776de471c72cb0600bfa13f
Nikhil  Chougule <nikhil.choughule@soroco.com>
2018-11-05 11:02:09 +0530

    build

---
+++ build/precache-manifest.f63656e19e19f41c0dec7606c9f1871b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bfa4e5b8155024abb885",
+    "url": "/static/js/main.bfa4e5b8.chunk.js"
+  },
+  {
+    "revision": "0d03e9cb66760a3aa79a",
+    "url": "/static/js/1.0d03e9cb.chunk.js"
+  },
+  {
+    "revision": "bfa4e5b8155024abb885",
+    "url": "/static/css/main.b78ea79d.chunk.css"
+  },
+  {
+    "revision": "9276db60849e7e3ad2f10f526fef8877",
+    "url": "/index.html"
+  }
+];
